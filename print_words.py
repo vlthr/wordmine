1e67df5c8e2d0903b55fb52b15bd0043ba107eb9
Valthor Halldorsson <halldorsson.v@gmail.com>
2017-03-28 08:47:20 +0000

    100k ascii

--- print_words.py
+++ print_words.py
@@ -7,19 +7,24 @@ db = sqlite3.connect('words.db')
 
 words = db.execute("select word from Words where length(word) = 5 OR length(word) == 6")
 
-latin_out = (open("words-latin.txt", "w+"), Counter())
-all_out = (open("words-all.txt", "w+"), Counter())
-ascii_out = (open("words-ascii.txt", "w+"), Counter())
+latin_out = (open("words-latin.txt", "w+"), set(), Counter())
+all_out = (open("words-all.txt", "w+"), set(), Counter())
+ascii_out = (open("words-ascii.txt", "w+"), set(), Counter())
+
+def add(out, word):
+    f, s, counter = out
+    s.add(word)
 
 def write(out, word):
-    f, counter = out
+    f, s, counter = out
     f.write(word)
-    f.write('\n')
-    for c in word.lower():
+    f.write("\n")
+    for c in word:
         counter[c] += 1
+
 latin_letters= {}
 def is_latin(uchr):
-    try: return latin_letters[uchr]
+    try: return uchr.isalpha() and latin_letters[uchr]
     except KeyError:
          return latin_letters.setdefault(uchr, 'LATIN' in ud.name(uchr))
 
@@ -36,14 +41,18 @@ for w in words:
     word = w[0].strip().lower()
     if re.search(r"[\d.,' ]", word): continue
     if len(word) != 5: continue
-    write(all_out, word)
+    add(all_out, word)
     if only_ascii_chars(word):
-        write(ascii_out, word)
+        add(ascii_out, word)
     if only_roman_chars(word):
-        write(latin_out, word)
+        add(latin_out, word)
 
-for f, counter in (ascii_out, latin_out, all_out):
+for f, s, counter in (ascii_out, latin_out, all_out):
+    for w in s:
+        write((f, s, counter), w)
     f.close()
+
+for f, s, counter in (ascii_out, latin_out, all_out):
     length = len(counter)
     print("Total count: {} characters".format(length))
     if length < 200:
